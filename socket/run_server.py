d19287e46d5fe5b7a58b4879acd996ad4c07c47a
MXG <micgriffin88@gmail.com>
2024-12-07 16:30:06 -0500

    simplified running server and client

---
+++ socket/run_server.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "cryptoSocket.asgi:application",
+        host="127.0.0.1",
+        port=8000,
+        ssl_keyfile="key.pem",
+        ssl_certfile="cert.pem",
+    )
